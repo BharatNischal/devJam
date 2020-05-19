3ffed399327a53d6ebd0c89747ae1747fc09c578
BharatNischal <33544814+BharatNischal@users.noreply.github.com>
2020-05-19 13:02:01 +0530

    Add files via upload

---
+++ createsampleProfile.js
@@ -0,0 +1,84 @@
+const Developer = require('./models/developer');
+
+const developer = {
+    personalInfo:{
+        firstName: "Bharat",
+        lastName: "Nischal",
+        title: "Student",
+    },
+    education:[{
+        schoolName:"Shiv Jyoti public school",
+        degree: "B Tech",
+        subject:"Computer Science",
+        from: Date.now(),
+        to:{
+          present:true
+        }
+    }],
+    experience:[{
+        company:"Cess",
+        position:"Technical Head",
+        from:Date.now(),
+        to:{
+          present:true
+        },
+        description: "Making Coding fun in the campus"
+    }],
+    languages:[{
+        name:"Javascript",
+        rating:4
+    },{
+        name:"C++",
+        rating:4
+    },{
+        name:"Python",
+        rating:2
+    }],
+    frontend:[{
+        name:"React",
+        rating:3
+    },{
+        name:"Bootstrap",
+        rating:3
+    },{
+        name:"HTML CSS",
+        rating:4
+    }],
+    backend:[{
+        name:"NodeJs",
+        rating:4
+    }],
+    database:[{
+        name:"MongoDB",
+        rating:3
+    },{
+        name:"MySQL",
+        rating:3
+    }],
+    tools:[{
+        name:"Git",
+        rating:3
+    },{
+        name:"Chrome Extension",
+        rating:2
+    }],
+    softSkills:[{
+        name:"Communication",
+        rating:3
+    }],
+    hobbies:["Cricket","Games"],
+    contact:{
+        email: "nischalbharat4819@gmail.com",
+        phone: 1234567890,
+        github: "github.com/BharatNischal",
+        youtube: ""
+    }
+}
+
+Developer.create(developer)
+  .then(profile=>{
+    console.log(profile);
+  })
+  .catch(err=>{
+    console.log(err);
+  })
