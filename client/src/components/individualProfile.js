af42e0a11c8e4d6564e5f926ef4576efff702b75
BHARAT <nischalbharat4819@gmail.com>
2020-05-20 16:03:10 +0530

    links on profiles page working

--- client/src/components/individualProfile.js
+++ client/src/components/individualProfile.js
@@ -1,21 +1,22 @@
 import React from "react";
+import {withRouter} from "react-router-dom";
 
 const IndividualProfile = (props)=>{
-
+    const {profilePic,personalInfo} = props.data;
     return (
       <div className="col-12 col-md-4 my-2" style={{height:"100%"}}>
         <div className="card mx-2" >
-            <img src="https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/indian_man_turban_sikh-512.png" className="card-img-top mx-auto d-block mt-1" style={{maxWidth:"100px"}} />
+            <img src={profilePic} className="card-img-top mx-auto d-block mt-1" style={{maxWidth:"100px"}} />
           <div className="card-body">
-            <h5 className="card-title">Bharat Nischal</h5>
-            <p className="card-text">Details................</p>
+            <h5 className="card-title">{`${personalInfo.firstName} ${personalInfo.lastName}`}</h5>
+            <p className="card-text">{personalInfo.title}</p>
             <div className="row">
               <div className="col-5">
-                <a className="btn btn-primary float-left ml-1 text-white w-100"> View </a>
+                <a className="btn btn-primary float-left ml-1 text-white w-100" onClick={()=>{props.history.push(`/profile/${personalInfo.firstName}/${props.data._id}`)}}> View </a>
               </div>
               <div className="col-2"></div>
               <div className="col-5">
-                <a className="btn float-right mr-1 text-white bgd-gradient b-none w-100"> Edit </a>
+                <a className="btn float-right mr-1 text-white bgd-gradient b-none w-100" onClick={()=>{props.history.push(`/editProfile/${personalInfo.firstName}/${props.data._id}`)}}> Edit </a>
               </div>
             </div>
           </div>
@@ -24,4 +25,4 @@ const IndividualProfile = (props)=>{
     );
 }
 
-export default IndividualProfile;
+export default withRouter(IndividualProfile);
