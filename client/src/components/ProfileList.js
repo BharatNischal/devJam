29be2b60544ccf96f824f60e6eac8a70cc9adfe8
BHARAT <nischalbharat4819@gmail.com>
2020-05-20 15:24:22 +0530

    profiles page added

--- client/src/components/ProfileList.js
+++ client/src/components/ProfileList.js
@@ -4,22 +4,29 @@ import axios from "axios";
 
 const ProfileList = (props)=>{
 
-      const [profiles,setProfiles] = useState([]);
+      const [profiles,setProfiles] = useState([{id:1},{id:2},{id:3}]);
 
-      useEffect(()=>{
-        const fetchProfiles = async () => {
-           const response = await axios.get(`http://localhost:8080/profiles`);
-           if(response.data){
-             setProfiles(response.data);
-           }
-        }
-        fetchProfiles();
-      },[]);
+      // useEffect(()=>{
+      //   const fetchProfiles = async () => {
+      //      const response = await axios.get(`http://localhost:8080/profiles`);
+      //      if(response.data){
+      //        setProfiles(response.data);
+      //      }
+      //   }
+      //   fetchProfiles();
+      // },[]);
+
+    const list = profiles.map(profile=>(
+                    <IndividualProfile key={profile._id} data={profile}/>
+                ))
 
     return (
-      profiles.map(profile=>(
-          <IndividualProfile key={profile._id} data={profile}/>
-      ))
+      <div className="container">
+        <h2 className="mt-5 text-white">Developers Profiles</h2>
+        <div className="row mt-5">
+          {list}
+        </div>
+      </div>
     );
 }
 
--- client/src/components/individualProfile.js
+++ client/src/components/individualProfile.js
@@ -3,8 +3,23 @@ import React from "react";
 const IndividualProfile = (props)=>{
 
     return (
-      <div>
-        Individual Profile Page
+      <div className="col-12 col-md-4 my-2" style={{height:"100%"}}>
+        <div className="card mx-2" >
+            <img src="https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/indian_man_turban_sikh-512.png" className="card-img-top mx-auto d-block mt-1" style={{maxWidth:"100px"}} />
+          <div className="card-body">
+            <h5 className="card-title">Bharat Nischal</h5>
+            <p className="card-text">Details................</p>
+            <div className="row">
+              <div className="col-5">
+                <a className="btn btn-primary float-left ml-1 text-white w-100"> View </a>
+              </div>
+              <div className="col-2"></div>
+              <div className="col-5">
+                <a className="btn float-right mr-1 text-white bgd-gradient b-none w-100"> Edit </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
 }
