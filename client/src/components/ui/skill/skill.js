5e6adee10cf5c72967223db8319cd990c6a471b1
Manjot Singh <manjotsingh16july@gmail.com>
2020-06-03 00:48:31 +0530

    technical skills section added

--- client/src/components/ui/skill/skill.js
+++ client/src/components/ui/skill/skill.js
@@ -13,22 +13,35 @@ const Skill = (props)=>{
     }catch(err){
         img= "noImg";
     }
-    
- return(
-    <div className="row mb-2">
-        <div className="col-4"> <div className="iconCont" >
-             <div className="innerIconCont"> 
-                {img==="noImg"?<p className="iconImg" > {props.icon} </p>:<img src={img} className="iconImg" alt="skill icon" /> }
+    const icon=(
+        <div className="col-4 col-sm-3 col-lg-2 col-xl-1 mb-2" style={{height:"90px"}}>
+        <div className="iconCont" style={{width:"80px",height:"80px"}}>
+             <div className="innerIconCont" style={{width:"60px",height:"60px"}}> 
+                {img==="noImg"?<p className="iconImg onyIcon" > {props.icon} </p>:<img src={img} className="iconImg onlyIcon" alt="skill icon" /> }
              </div> 
-        </div></div>
-        <div className="col-8">
-            <Rating val={props.rating} size="18px" />
-            {props.experience?<h5 className="pl-2" > {props.experience} Years</h5>:null}
-            {props.description?<p className="pl-2">{props.description}</p>:null}
-            
-
         </div>
-    </div>);
+        </div>
+    ); 
+ return(
+     <React.Fragment>
+         {props.onlyIcon?icon:(
+             <div className="row mb-2">
+             <div className="col-4"> <div className="iconCont" >
+                  <div className="innerIconCont"> 
+                     {img==="noImg"?<p className="iconImg" > {props.icon} </p>:<img src={img} className="iconImg" alt="skill icon" /> }
+                  </div> 
+             </div></div>
+             <div className="col-8">
+                 <Rating val={props.rating} size="18px" />
+                 {props.experience?<h5 className="pl-2" > {props.experience} Years</h5>:null}
+                 {props.description?<p className="pl-2">{props.description}</p>:null}
+                 
+     
+             </div>
+         </div>
+         )}
+     </React.Fragment>
+    );
 };
 
 export default Skill;
