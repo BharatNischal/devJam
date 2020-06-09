2ac89f26fc81851255a9209bd8f9ef9940c18858
BHARAT <nischalbharat4819@gmail.com>
2020-06-09 15:42:43 +0530

    update videoplayer

--- client/src/components/videoPlayer.js
+++ client/src/components/videoPlayer.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from "react";
+import React,{useState,useEffect,useRef} from "react";
 import { Player, ControlBar,BigPlayButton, LoadingSpinner,ReplayControl,ForwardControl,PlaybackRateMenuButton } from 'video-react';
 import axios from "axios";
 
@@ -6,15 +6,18 @@ const VideoPlayer = (props)=>{
 
     // Control the quality of video
     const [qlt,setQlt] = useState(70);
+    const [timeWatched,setTimeWatched] = useState(0);
     // Used to authorize a video to user for a small window
     const [authorize,setAuthorize] = useState(false);
-
+    const playerRef = useRef(null);
     // Authorize user to access a video for a small window (To avoid downloading)
     useEffect(()=>{
       axios.post('/video/access')
         .then(res=>{
           if(res.data.success){
             setAuthorize(true);
+            // playerRef.current.getState().player.play();
+            console.log(playerRef.current.getState());
           }else{
             console.log(res.data.msg);
           }
@@ -25,12 +28,19 @@ const VideoPlayer = (props)=>{
     },[])
 
     useEffect(()=>{
+      const time = playerRef.current.getState().player.currentTime;
       setAuthorize(false);
       console.log("called",qlt);
+      setTimeWatched(playerRef.current.getState().player.currentTime);
+      console.log(playerRef.current.getState().player.currentTime);
       axios.post('/video/access')
         .then(res=>{
           if(res.data.success){
             setAuthorize(true);
+            playerRef.current.load(`/video/${props.videoId}/${qlt}`);
+            console.log(time,"sec");
+            playerRef.current.forward(time);
+            // playerRef.current.play();
           }else{
             console.log(res.data.msg);
           }
@@ -41,8 +51,8 @@ const VideoPlayer = (props)=>{
     },[qlt])
 
     return (
-          {authorize}?<div style={{height:"60vh"}}>
-          <Player>
+        <div style={{height:"60%"}}>
+          <Player fluid={false} height={300} width={600} ref={playerRef}>
             <source src={`/video/${props.videoId}/${qlt}`} />
             <ControlBar autoHide={false}>
               <PlaybackRateMenuButton rates={[2, 1, 0.5, 0.1]} />
@@ -53,7 +63,7 @@ const VideoPlayer = (props)=>{
             <LoadingSpinner/>
           </Player>
           <button onClick={()=>setQlt(30)}>Qlt low</button>
-          </div>:<div></div>
+        </div>
     );
 }
 
