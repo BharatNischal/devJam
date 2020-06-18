839c670ef9c48fd084150ad5a4ef467b51a6c5b1
Manjot Singh <manjotsingh16july@gmail.com>
2020-06-19 01:14:14 +0530

    timestamp added on comments

--- client/src/components/markingSystem/submission.js
+++ client/src/components/markingSystem/submission.js
@@ -81,6 +81,31 @@ function SubmissionPage(props) {
             console.log(err.message);
           })
     }
+    // Function to show relative time w.r.t current time on comments
+  function timeDifference(previous) {
+
+    var msPerMinute = 60 * 1000;
+    var msPerHour = msPerMinute * 60;
+    var msPerDay = msPerHour * 24;
+    var msPerMonth = msPerDay * 30;
+    var msPerYear = msPerDay * 365;
+    var current = Date.now()
+    var elapsed = current - previous;
+
+    if (elapsed < msPerMinute) {
+         return Math.round(elapsed/1000) + ' seconds ago';
+    }else if (elapsed < msPerHour) {
+         return Math.round(elapsed/msPerMinute) + ' minutes ago';
+    }else if (elapsed < msPerDay ) {
+         return Math.round(elapsed/msPerHour ) + ' hours ago';
+    }else if (elapsed < msPerMonth) {
+         return Math.round(elapsed/msPerDay) + ' days ago';
+    }else if (elapsed < msPerYear) {
+         return Math.round(elapsed/msPerMonth) + ' months ago';
+    }else {
+         return Math.round(elapsed/msPerYear ) + ' years ago';
+    }
+}
 
     const options=[];
     if(deliverable){
@@ -209,7 +234,7 @@ function SubmissionPage(props) {
                                 <div className="row mt-3" style={{justifyContent:"center"}}>
                                     <div className="profile-pic rounded-circle border " style={{height:"50px",width:"50px",overflow:"hidden"}}><img src={submissions[curIndex].userId.profilePic || UserImg}  className="rounded-circle  responsive-img" /></div>
                                     <div className="col-9">
-                                        <h6 className="text-left"><b>{submissions[curIndex].userId.name}</b></h6>
+                                        <h6 className="text-left"><b>{submissions[curIndex].userId.name}<span className="ml-2" style={{fontSize:"0.8em"}}>{timeDifference(new Date(submissions[curIndex].submissionId.timestamp))}</span></b></h6>
                                         <div className="text-left" style={{lineHeight:"100%"}}>
                                             {submissions[curIndex].submissionId.comment}
                                         </div>
