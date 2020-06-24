126f7197749b424561946cfa9144f724df156e9f
Manjot Singh <manjotsingh16july@gmail.com>
2020-06-24 15:17:36 +0530

    issue solved

--- client/src/components/tests/LiveTests/LiveTest.js
+++ client/src/components/tests/LiveTests/LiveTest.js
@@ -3,6 +3,7 @@ import TopBar from "../../learnerPlatform/TopBar";
 import "./liveTest.css";
 import StartPage from './startPage';
 import Question from './question';
+import axios from "axios";
 
 const lorem ="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
 
