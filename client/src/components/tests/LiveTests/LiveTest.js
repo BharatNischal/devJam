d457b83ed2d8f7b2618063887091e78969b008e0
Manjot Singh <manjotsingh16july@gmail.com>
2020-06-24 12:55:39 +0530

    liveTest page done

--- client/src/components/tests/LiveTests/LiveTest.js
+++ client/src/components/tests/LiveTests/LiveTest.js
@@ -2,6 +2,7 @@ import React from 'react';
 import TopBar from "../../learnerPlatform/TopBar";
 import "./liveTest.css";
 import StartPage from './startPage';
+import Question from './question';
 
 const lorem ="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
 
@@ -13,7 +14,11 @@ function LiveTest(props) {
             <div className="bgwhiteoverlay"></div>
             
                 <div className="frame p-4">
-                    <StartPage title="Maths Test" instruction={lorem} duration={30}  />
+                    
+                    {/* Start Page Should only be shown when user has not started test yet, not when he refresh page */}
+                    {/* <StartPage title="Maths Test" instruction={lorem} duration={30}  />   */}
+                    
+                    <Question/>
                 </div>
             
         </React.Fragment>
