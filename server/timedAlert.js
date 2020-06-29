d6c9cd76f4f5bb37afc281df8e20e171aeb3e49d
BHARAT <nischalbharat4819@gmail.com>
2020-06-29 21:40:09 +0530

    timed alert issue fixed

--- server/timedAlert.js
+++ server/timedAlert.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const db=require("../models/index");
-const mailFunction = require("../mail");
+const db=require("./models/index");
+const mailFunction = require("./mail");
 
 function formatDate(date) {
   var d = new Date(date),
@@ -8,9 +8,9 @@ function formatDate(date) {
       day = '' + d.getDate(),
       year = d.getFullYear();
 
-  if (month.length < 2) 
+  if (month.length < 2)
       month = '0' + month;
-  if (day.length < 2) 
+  if (day.length < 2)
       day = '0' + day;
 
   return [year, month, day].join('-');
@@ -20,7 +20,7 @@ try{
   var date = new Date();
     var h18 = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 18);
     if(date.getHours() >= 18) {
-        h18.setDate(h17.getDate()+1);
+        h18.setDate(h18.getDate()+1);
     }
     h18 = h18.getTime();
     var diff = h18 - date.getTime();
@@ -36,13 +36,13 @@ catch(err){
 
 function dailyInterval(){
   timeAlert();
-  setInterval(timeAlert,24*3600000); 
+  setInterval(timeAlert,24*3600000);
 }
 
 function timeAlert() {
   var month = new Date().getMonth()+1;
   db.Course.find({status:"Published",startMonth:{$lte:month},endMonth:{$gte:month}})
-    .populate(['students','events.video','events.deliverable','events.test','events.event'])
+    .populate(['students','events.items.video','events.items.deliverable','events.items.test','events.items.event'])
       .then(courses=>{
           let promises = [];
           let newStudents=[];
@@ -52,16 +52,18 @@ function timeAlert() {
             var tomorrow = new Date(date.getFullYear(), date.getMonth(), date.getDate()+1,0);
             let listedEvents = [];
             course.events.forEach(e=>{
-              if(e.date.substr(0,10)==formatDate(tomorrow)){
+              console.log("db",e.date.toISOString().substr(0,10),"format",formatDate(tomorrow));
+              if(e.date.toISOString().substr(0,10)==formatDate(tomorrow)){
                   listedEvents = e.items.map(item=>{
+                    console.log("Item",item);
                     if(item.video){
-                      return item.video.title;
+                      return "Video "+item.video.title;
                     }else if(item.deliverable){
-                      return item.deliverable.title;
+                      return "Deliverable "+item.deliverable.title;
                     }else if(item.test){
-                      return item.test.title;
+                      return "Test "+item.test.title;
                     }else{
-                      return item.event.title;
+                      return "Event "+item.event.title;
                     }
                   });
               }
@@ -74,7 +76,7 @@ function timeAlert() {
                     from: '"Learner Platform" <manjotsingh16july@gmail.com>', // sender address (who sends)
                     to: student.username, // list of receivers (who receives)
                     subject: 'Daily Reminder', // Subject line
-                    text: `Dear student your schedule for tommor for Course${course.title} is ${text}`
+                    text: `Dear student your schedule for tomorrow for Course ${course.title} is \n${text}`
                   };
 
                   mailFunction.mailFunction(msg,(err,info)=>{
@@ -85,7 +87,7 @@ function timeAlert() {
 
                   promises.push(db.Notification.create({
                     title:`Dear student your schedule for tommor for Course${course.title} is ${text}`,
-                    link:fullUrl,
+                    link:"",
                     type:"course"
                   }));
                   newStudents.push(student);
