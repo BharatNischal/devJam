221119ea2be1dc74ba6cd7f734530d98e67a8725
Manjot Singh <manjotsingh16july@gmail.com>
2020-07-01 19:33:43 +0530

    limits added

--- client/src/components/codingPlatform/addQuestion.js
+++ client/src/components/codingPlatform/addQuestion.js
@@ -6,6 +6,7 @@ import TestCases from './TestCases';
 import InputOutput from './inputOutput';
 import SampleCases from './sampleCases';
 import axios from 'axios';
+import Limits from './limits';
 
 
 function AddQuestion(props) {
@@ -86,37 +87,7 @@ function AddQuestion(props) {
                             <TestCases />
                         :null}
                         {activeTab=="limits"?
-                            <React.Fragment>
-                                <h2 className="topicTitle text-pink mb-2"  ><b>Constraints</b></h2>
-                                <Editor
-                                    toolbarClassName="toolbarClassName"
-                                    wrapperClassName="constraintWrapper"
-                                    editorClassName="editorClassName"
-                                    editorState={sampleEditorState}
-                                    onEditorStateChange={(editorState)=>setSampleEditorState(editorState)}
-                                    toolbar={{
-                                        options: ['inline', 'blockType', 'list', 'textAlign', 'link', 'emoji', 'remove', 'history']
-                                    }}
-                                />
-                                <div className=" row my-4">
-                                    <div className="col-md-6">
-                                        <h4 className="text-pink"> <b>Time Limit (Seconds) </b> </h4>
-                                        <div className="form-group input-group px-lg-4">
-                                            <div className="input-group-prepend rounded bg-grad text-white pl-3 pr-3 pt-2 f-20 " > <i className="fa fa-clock"></i> </div>
-                                            <input type="number" className="form-control"   placeholder="Enter Time" />
-                                        </div>
-                                    </div>
-                                    <div className="col-md-6">
-                                        <h4 className="text-pink"> <b>Memory Limit (KB) </b> </h4>
-                                        <div className="form-group input-group px-lg-4">
-                                            <div className="input-group-prepend rounded bg-grad text-white pl-3 pr-3 pt-2 f-20 " > <i className="fas fa-memory"></i> </div>
-                                            <input type="number" className="form-control"   placeholder="Enter Memory Limit" />
-                                        </div>
-                                    </div>
-
-                                </div>
-                                
-                            </React.Fragment>
+                            <Limits/>
                         :null}
                     </div>
                 </div>
