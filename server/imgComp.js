12c52d322461ad5035d59eb8ca9a06eb7376b1f0
Manjot Singh <manjotsingh16july@gmail.com>
2020-07-04 11:44:56 +0530

    img save logic added

--- server/imgComp.js
+++ server/imgComp.js
@@ -1,23 +1,7 @@
-const deepai = require('deepai'); // OR include deepai.min.js as a script tag in your HTML
-const          express = require('express'),
-                app = express();
-const fs = require('fs');
-deepai.setApiKey('3d0a52e5-8f98-4c55-a854-c37b3cda545a');
-
-try{
-(async function() {
-    var resp = await deepai.callStandardApi("image-similarity", {
-            image1: "https://res.cloudinary.com/bharatnischal/image/upload/v1568002016/t9l7poh7clueeprm4vra.jpg",
-            image2: "https://res.cloudinary.com/bharatnischal/image/upload/v1565425311/vqrejhytby9sjmbict6r.jpg",
-    });
-    console.log(resp);
-})()
-}
-catch(err){
-  console.log(err.message);
-}
-
-const port = process.env.PORT || 8000;
-app.listen(port,()=>{
-  console.log("Listening on port ",port);
-});
+const nodeHtmlToImage = require('node-html-to-image');
+ 
+nodeHtmlToImage({
+  output: './image.png',
+  html: '<html><style> h1{color:red;} </style><body><h1>Hello world!</h1></body></html>'
+})
+  .then(() => console.log('The image was created successfully!'))
